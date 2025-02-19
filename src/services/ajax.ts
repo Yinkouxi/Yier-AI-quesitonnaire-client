252dfa0c2e4c11efe4c1f5312cb68e19c47f153d
yinkouxi <3405176636@qq.com>
2025-02-19 21:04:24 +0800

    feat: version 1.0 答卷基础内容搭建

---
+++ src/services/ajax.ts
@@ -0,0 +1,19 @@
+const HOST = "http://localhost:3000"; //nest服务端
+
+export async function get(url: string) {
+  const res = await fetch(`${HOST}${url}`);
+  const data = res.json();
+  return data;
+}
+
+export async function post(url: string, body: any) {
+  const res = await fetch(`${HOST}${url}`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  const data = res.json();
+  return data;
+}
